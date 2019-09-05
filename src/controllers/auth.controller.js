7969172a58a938b64e1444d4e5f6b86829ee3c7f
unknown <jhiester@gmail.com>
2019-09-04 22:39:46 -0700

    getting auth to work

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+const { AsyncRouter } = require('express-async-router');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+const controller = AsyncRouter();
+
+const singUpValidators = [
+  check('username')
+    .exists()
+    .isLength({ min: 3, max: 32 }),
+  check('password')
+    .exists()
+    .isLength({ min: 8, max: 64 }),
+  check('passwordCheck')
+    .exists()
+    .isLength({ min: 8, max: 64 })
+];
+
+const loginValidators = [
+  check('username')
+    .exists()
+    .isLength({ min: 3, max: 32 }),
+  check('password')
+    .exists()
+    .isLength({ min: 8, max: 64 })
+];
+
+const sanitizeUser = user => ({
+  ...user.toJSON(),
+  password: undefined
+});
+
+controller.post('/sign-up', [...singUpValidators], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).send({ errors: errors.array() });
+  }
+
+  const { username, password, passwordCheck } = req.body;
+
+  const userFound = await User.findOne({ username });
+
+  if (userFound) {
+    return res.status(422).send({ error: 'user already exists' });
+  }
+
+  if (password != passwordCheck) {
+    return res.status(422).send({ error: 'mismatch between password and password check' });
+  }
+
+  const user = new User();
+  user.username = username;
+  user.password = bcrypt.hashSync(password, 4);
+  await user.save();
+
+  res.send(sanitizeUser(user)).status(200);
+});
+
+controller.post('/login', [...loginValidators], async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.sendStatus(403);
+
+  const correctPassword = bcrypt.compareSync(password, user.password);
+  if (!correctPassword) return res.sendStatus(403);
+
+  const token = jwt.sign(sanitizeUser(user), process.env.JWT_TOKEN, {
+    expiresIn: '7 days'
+  });
+
+  res.status(200).send({ token });
+});
+
+module.exports = controller;
--- test/controllers/user.controller.test.js
+++ test/controllers/user.controller.test.js
@@ -1,32 +1,31 @@
-const chai = require("chai");
+const chai = require('chai');
 const { expect } = chai;
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const { app } = require("../../src/server");
+const { app } = require('../../src/server');
 
-describe("user.controller.js", () => {
-    it("POST /signup: allows valid users to sign up", async () => {
-        const response = await chai
-          .request(app)
-          .post("/signup")
-          .send({
-              username: "testUser",
-              password: "password123",
-              passwordValidator: "password123",
-          })
-        expect(response.status).to.equal(200)
-    })
-    it("POST /signup: doesnt allow duplicate usernames to sign up", async () => {
-        const response = await chai
-        .request(app)
-        .post("/signup")
-        .send({
-            username: "testUser",
-            password: "passwordalso",
-            passwordValidator: "passworldalso"
-        })
-        expect(response.status).to.equal(400)
-    })
-    it("POST /signup: doesn't allow signup with")
- 
-})
+describe('user.controller.js', () => {
+  it('POST /signup: allows valid users to sign up', async () => {
+    const response = await chai
+      .request(app)
+      .post('/signup')
+      .send({
+        username: 'testUser',
+        password: 'password123',
+        passwordCheck: 'password123'
+      });
+    expect(response.status).to.equal(200);
+  });
+  it('POST /signup: doesnt allow duplicate usernames to sign up', async () => {
+    const response = await chai
+      .request(app)
+      .post('/signup')
+      .send({
+        username: 'testUser',
+        password: 'passwordalso',
+        passwordCheck: 'passworldalso'
+      });
+    expect(response.status).to.equal(400);
+  });
+  it("POST /signup: doesn't allow signup with");
+});
